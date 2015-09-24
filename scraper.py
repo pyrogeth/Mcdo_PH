514ff8e83030c4a05414750a2e6997401f135899
pyrogeth <greenstaples30@gmail.com>
2015-09-24 16:52:56 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,26 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import simplejson
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+# retrieve a page
+base_url = 'http://search.twitter.com/search.json?q='
+q = 'neardeath'
+options = '&rpp=100&page='
+page = 1
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+while 1:
+    try:
+        url = base_url + q + options + str(page)
+        html = scraperwiki.scrape(url)
+        #print html
+        soup = simplejson.loads(html)
+        for result in soup['results']:
+            data = {}
+            data['id'] = result['id']
+            data['text'] = result['text']
+            data['from_user'] = result['from_user']
+            # save records to the datastore
+            scraperwiki.datastore.save(["id"], data) 
+        page = page + 1
+    except:
+        print str(page) + ' pages scraped'
+        break
